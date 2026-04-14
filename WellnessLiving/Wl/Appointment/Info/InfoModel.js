b555641c045a1111501d2121051f73c77abdb72d
helgikropp <oleh.l.didenko@gmail.com>
2026-04-14 11:09:53 +0300

    wl-80206: Add location key to \Wl\Appointment\Info\InfoApi

--- WellnessLiving/Wl/Appointment/Info/InfoModel.js
+++ WellnessLiving/Wl/Appointment/Info/InfoModel.js
@@ -135,6 +135,14 @@ function Wl_Appointment_Info_InfoModel()
   this.k_appointment = "0";
 
   /**
+   * Location key.
+   *
+   * @get result
+   * @type {string}
+   */
+  this.k_location = undefined;
+
+  /**
    * The Purchase Option used to purchase the appointment.
    *
    * @get result
@@ -208,7 +216,7 @@ WlSdk_ModelAbstract.extend(Wl_Appointment_Info_InfoModel);
  */
 Wl_Appointment_Info_InfoModel.prototype.config=function()
 {
-  return {"a_field": {"a_next": {"get": {"result": true}},"a_previous": {"get": {"result": true}},"a_question": {"get": {"result": true}},"a_resource": {"get": {"result": true}},"a_shop_product_option": {"get": {"result": true}},"dt_date_local": {"get": {"result": true}},"i_duration": {"get": {"result": true}},"i_index": {"get": {"result": true}},"id_appointment_pay": {"get": {"result": true}},"k_appointment": {"get": {"get": true}},"k_login_promotion": {"get": {"result": true}},"k_resource": {"get": {"result": true}},"k_resource_type": {"get": {"result": true}},"k_service": {"get": {"result": true}},"k_service_category": {"get": {"result": true}},"k_staff": {"get": {"result": true}},"text_title": {"get": {"result": true}},"uid_appointment": {"get": {"result": true}}}};
+  return {"a_field": {"a_next": {"get": {"result": true}},"a_previous": {"get": {"result": true}},"a_question": {"get": {"result": true}},"a_resource": {"get": {"result": true}},"a_shop_product_option": {"get": {"result": true}},"dt_date_local": {"get": {"result": true}},"i_duration": {"get": {"result": true}},"i_index": {"get": {"result": true}},"id_appointment_pay": {"get": {"result": true}},"k_appointment": {"get": {"get": true}},"k_location": {"get": {"result": true}},"k_login_promotion": {"get": {"result": true}},"k_resource": {"get": {"result": true}},"k_resource_type": {"get": {"result": true}},"k_service": {"get": {"result": true}},"k_service_category": {"get": {"result": true}},"k_staff": {"get": {"result": true}},"text_title": {"get": {"result": true}},"uid_appointment": {"get": {"result": true}}}};
 };
 
 /**
